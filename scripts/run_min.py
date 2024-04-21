0bdc02126f6c04f95e126b02a0eda874fa7917c8
peppapiggyme <chambowen@126.com>
2024-04-21 11:20:37 +0800

    more realistic example of min/max

--- scripts/run_min.py
+++ scripts/run_min.py
@@ -8,12 +8,12 @@ x = np.random.rand(length) - 0.5
 vx = c2w.Vectorf(x.tolist(), length)
 
 start = perf_counter_ns()
-val = np.max(x)
+val = np.min(x)
 end = perf_counter_ns()
-print(f"Max = {val:.4f}, Duration = {(end - start) * 1e-6:.4f} ms")
+print(f"Min = {val:.4f}, Duration = {(end - start) * 1e-6:.4f} ms")
 
 start = perf_counter_ns()
-val, idx = vx.max()
+val, idx = vx.min()
 end = perf_counter_ns()
-print(f"Max = {val:.4f}, Idx = {idx}, Duration = {(end - start) * 1e-6:.4f} ms")
+print(f"Min = {val:.4f}, Idx = {idx}, Duration = {(end - start) * 1e-6:.4f} ms")
 
